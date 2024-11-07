dfd9a0d900e72a99465d0ced606b001974085103
artem <artialbk@gmail.com>
2024-11-07 06:46:33 +0300

    sdf

--- book.js
+++ book.js
@@ -7,7 +7,7 @@
 //    - getInfo() — возвращает строку, содержащую информацию о книге в формате: "Название: 'Название книги', Автор: 'Автор книги', Год: 'Год выпуска'".
 
 class Book {
-    constructor(title, author, year) {
+    constructor(title,author,year) {
         this.title = title
         this.author = author
         this.year = year
@@ -26,11 +26,39 @@ class Book {
     }
 }
 
-let book1 = new Book("1984", "Дж. Оруэлл", 1984)
+class EBook extends Book{
+    constructor(title,author,year,format,size){
+        super(title,author,year)
+        this.format=format
+        this.size=size
+    }
+
+    getInfo(){
+        super.getInfo()
+        return `
+        Формат: '${this.format}', 
+        Размер: '${this.size}'
+        `
+    }
+
+    set newSize(newSize){
+        this.size = newSize
+    }
+}
+
+
+
+let book1 = new Book("1984","Оруэлл",1984)
 let book2 = new Book("Преступление и наказание", "Ф.М. Достоевский", 1880)
+let eBook = new EBook("том сойер", "М.твенн", 1950, "pdf", "247 Mb" )
+
 
 console.log(book1.getInfo())
 console.log(book2.getInfo())
+console.log(eBook.getInfo())
 
 book2.newTitle = "Идиот"
 console.log(book2.getInfo())
+
+eBook.newSize = "222 MB"
+console.log(eBook.getInfo())
--- transport.js
+++ transport.js
@@ -1,76 +1,76 @@
-class Transport {
-    constructor(pass) {
-        this.passengersCount = pass
-    }
+// class Transport {
+//     constructor(pass) {
+//         this.passengersCount = pass
+//     }
 
-    get passengers() {
-        return this.passengersCount
-    }
+//     get passengers() {
+//         return this.passengersCount
+//     }
 
-    set passengers(pass) {
-        this.passengersCount = pass
-    }
+//     set passengers(pass) {
+//         this.passengersCount = pass
+//     }
 
-    move() {
-        console.log("Я еду")
-    }
+//     move() {
+//         console.log("Я еду")
+//     }
 
-    signal() {
-        console.log("Я бибикаю")
-    }
-}
+//     signal() {
+//         console.log("Я бибикаю")
+//     }
+// }
 
-class Car extends Transport {
-    constructor() {
-        super(5)
-    }
-}
+// class Car extends Transport {
+//     constructor() {
+//         super(5)
+//     }
+// }
 
-class Boat extends Transport {
-    constructor(pass) {
-        super(pass)
-    }
+// class Boat extends Transport {
+//     constructor(pass) {
+//         super(pass)
+//     }
 
-    move() {
-        return "Я хожу по морям"
-    }
-}
+//     move() {
+//         return "Я хожу по морям"
+//     }
+// }
 
-class Train extends Transport {
-    constructor(pass) {
-        super(pass)
-    }
-}
+// class Train extends Transport {
+//     constructor(pass) {
+//         super(pass)
+//     }
+// }
 
-class Plane extends Transport {
-    constructor(pass) {
-        super(pass)
-    }
+// class Plane extends Transport {
+//     constructor(pass) {
+//         super(pass)
+//     }
 
-    move() {
-        return "Я летаю"
-    }
+//     move() {
+//         return "Я летаю"
+//     }
 
-    signal() {
-        console.log(`Моргают ${this.paintLighters()} цвета`)
-    }
+//     signal() {
+//         console.log(`Моргают ${this.paintLighters()} цвета`)
+//     }
 
-    paintLighters() {
-        return "зеленый и красный"
-    }
-}
+//     paintLighters() {
+//         return "зеленый и красный"
+//     }
+// }
 
-let car = new Car()
-let boat = new Boat(12)
-let train = new Train(120)
-let plane = new Plane(200)
+// let car = new Car()
+// let boat = new Boat(12)
+// let train = new Train(120)
+// let plane = new Plane(200)
 
-car.passengers = 8
+// car.passengers = 8
 
-console.log(`
-в автомобиле ${car.passengers} пассажиров
-в лодке ${boat.passengers} пассажиров
-в поезде ${train.passengers} пассажиров
-в самолете ${plane.passengers} пассажиров
-`)
+// console.log(`
+// в автомобиле ${car.passengers} пассажиров
+// в лодке ${boat.passengers} пассажиров
+// в поезде ${train.passengers} пассажиров
+// в самолете ${plane.passengers} пассажиров
+// `)
 
